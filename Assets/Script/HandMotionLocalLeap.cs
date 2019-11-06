eb6d0aceb8a9cd714ffb805702cf8a27d62f050a
Prakash Verma <37665319+cartoonistprakash@users.noreply.github.com>
2019-11-07 00:03:10 +0100

    Initial commit

---
+++ Assets/Script/HandMotionLocalLeap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Valve.VR;
+using UnityEngine.XR;
+/// <summary>
+/// This functionality works on the local player only. It hides the Right IK arms when right leap bone structure is visible.
+/// </summary>
+public class HandMotionLocalLeap : NetworkBehaviour
+{
+    
+    public GameObject RightArmBicep;/*! This Game object takes the game object in unity editor representing Right Upper Arm */
+    public GameObject RightArmElbow;/*! This Game object takes the game object in unity editor representing Right Lower Arm */
+ /// <summary>
+ /// Update is called once per frame.
+ /// It hides the Right IK arms when right leap bone structure is visible.
+ /// </summary>
+                        
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        RightArmBicep.active = true;
+        RightArmElbow.active = true;
+        
+
+        ///hiding the right arm 
+        if (GameObject.Find("Right Interaction Hand Contact Bones").active == true)
+        {
+            RightArmBicep.active = false;
+            RightArmElbow.active = false;
+           
+            
+        }       
+                  
+
+    }
+}
